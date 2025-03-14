a87467ce091117c63e20afc837b8a507960417ba
Cosmin Marcu <marcucosmin3@yahoo.com>
2025-03-14 22:15:08 +0200

    Update firebase private key to come from local json file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,25 +1,10 @@
 import { credential } from "firebase-admin"
 import { getAuth } from "firebase-admin/auth"
 import { initializeApp } from "firebase-admin/app"
-import { config as dotenvConfig } from "dotenv"
 import { getFirestore } from "firebase-admin/firestore"
 
-const isProduction = process.env.NODE_ENV === "production"
-
-if (!isProduction) {
-  dotenvConfig()
-}
-
-const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY
-
-if (!firebasePrivateKey) {
-  throw new Error("FIREBASE_PRIVATE_KEY is not set")
-}
-
-const parsedFirebasePrivateKey = JSON.parse(firebasePrivateKey)
-
 const firebaseApp = initializeApp({
-  credential: credential.cert(parsedFirebasePrivateKey)
+  credential: credential.cert("firebase-private-key.json")
 })
 
 export const firestore = getFirestore(firebaseApp)
--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express"
+import { config as dotenvConfig } from "dotenv"
 
 import { handleGetRequest } from "./services/get"
 import { handleDeleteRequest } from "./services/delete"
+
+dotenvConfig()
 
 export const usersRouter = Router()
 
