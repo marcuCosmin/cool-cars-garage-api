b091347a4138daaad4b67b97e579de9ffcccb0cd
Cosmin Marcu <marcucosmin3@yahoo.com>
2025-05-29 20:49:41 +0300

    Update the mail route to properly limit rate the post request & send the error message when the limit is excedeed

--- src/routes/mail/index.ts
+++ src/routes/mail/index.ts
@@ -7,14 +7,14 @@ import { handlePostRequest } from "./services/post"
 const contactRateLimiter = rateLimit({
   windowMs: 60 * 1000,
   max: 3,
-  message: { error: "Too many requests, please try again later." }
+  handler: (_, res) =>
+    res
+      .status(429)
+      .json({ error: "Too many requests, please try again later." })
 })
 
 export const mailRouter = Router()
 
-mailRouter.use(
-  contactRateLimiter,
-  cors({ origin: process.env.MAIL_ALLOWED_ORIGIN })
-)
-mailRouter.options("/")
+mailRouter.use(cors({ origin: process.env.MAIL_ALLOWED_ORIGIN }))
+mailRouter.options("/", contactRateLimiter)
 mailRouter.post("/", handlePostRequest)
